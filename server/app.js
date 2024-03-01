708f5df95697c8f65ae60031847e5f6e8485bb56
Fanny Holmström <fannyholmstrom@icloud.com>
2024-03-01 11:24:36 +0100

    chore: change listen port

--- server/app.js
+++ server/app.js
@@ -24,4 +24,7 @@ io.on('connection', (socket) => {
   })
 })
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
+
